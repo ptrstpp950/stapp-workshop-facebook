5c73a6dedf79653188b9e190a209705e9b5c892b
Piotr Stapp <piotr.stapp@finai.com>
2019-04-10 14:58:10 +0200

    fix(tests): fix tests imports

--- src/app/posts/pages/post-page/post-page.component.spec.ts
+++ src/app/posts/pages/post-page/post-page.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostPageComponent } from './post-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('PostPageComponent', () => {
   let component: PostPageComponent;
@@ -8,6 +11,7 @@ describe('PostPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ SharedModule, HttpClientTestingModule, RouterTestingModule ],
       declarations: [ PostPageComponent ]
     })
     .compileComponents();
--- src/app/users/pages/user-page/user-page.component.spec.ts
+++ src/app/users/pages/user-page/user-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserPageComponent } from './user-page.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('UserPageComponent', () => {
   let component: UserPageComponent;
@@ -8,7 +10,11 @@ describe('UserPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserPageComponent ]
+      declarations: [ UserPageComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
